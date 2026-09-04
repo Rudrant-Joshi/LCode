78b9b3e2a32db53cbd13a3f599f7e39abb8d3174
Rudrant Joshi <rudrant.joshi@gmail.com>
2026-09-04 11:29:38 +0530

    Time: 3 ms (25.61%), Space: 54 MB (86.22%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -1,10 +1,13 @@
 var intersection = function(nums1, nums2) {
-    let arr = [];
-
-    for (let i = 0; i < nums1.length; i++) {
-        if (nums2.includes(nums1[i]) && !arr.includes(nums1[i])) {
-            arr.push(nums1[i]);
+    let arr= []
+    let x = 0
+    for(let i=0; i<nums1.length; i++){
+      if(nums2.includes(nums1[i]) == true) {
+        if(arr.includes(nums1[i])!= true){
+          arr[x] = nums1[i]
+          x++ 
         }
+      }
     }
-    return arr;
+    return arr
 };
