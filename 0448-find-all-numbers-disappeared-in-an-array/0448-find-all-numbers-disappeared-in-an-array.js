58ccdf74e79b7e29e07416d0ecbf74bc7477098b
Rudrant Joshi <rudrant.joshi@gmail.com>
2026-09-08 17:00:09 +0530

    Time: 18 ms (50.51%), Space: 76.1 MB (36.46%) - LeetHub

---
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -0,0 +1,10 @@
+var findDisappearedNumbers = function(nums) {
+    let set = new Set(nums);
+    let arr = [];
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (!set.has(i))  arr.push(i);
+    }
+
+    return arr;
+};
