875ccb5ae2af8ec7dfc72f97ab82337e4be0dda8
Rudrant Joshi <rudrant.joshi@gmail.com>
2026-09-09 21:59:33 +0530

    Time: 19 ms (35.69%), Space: 77.4 MB (14.47%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.js
@@ -0,0 +1,11 @@
+var findDuplicate = function(nums) {
+    let set = new Set();
+
+    for (let i = 0; i < nums.length; i++) {
+        if (set.has(nums[i])) {
+            return nums[i];
+        }
+
+        set.add(nums[i]);
+    }
+};
