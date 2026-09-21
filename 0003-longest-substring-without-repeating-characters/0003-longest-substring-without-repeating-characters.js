b8dcf51813f04bc5b785aaa0b46ab27e05a6cc1e
Rudrant Joshi <rudrant.joshi@gmail.com>
2026-09-22 01:22:16 +0530

    Time: 100 ms (22.6%), Space: 63.9 MB (11.85%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -0,0 +1,19 @@
+var lengthOfLongestSubstring = function(s) {
+    let set = new Set();
+    let max = 0;
+    let left = 0;
+
+    for (let right = 0; right < s.length; right++) {
+
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+
+        set.add(s[right]);
+
+        max = Math.max(max, set.size);
+    }
+
+    return max;
+};
