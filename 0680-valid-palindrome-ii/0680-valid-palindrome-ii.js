d180ddf3281e95a29a6e5a3076d7887c2a9aba88
Rudrant Joshi <rudrant.joshi@gmail.com>
2026-09-25 01:43:42 +0530

    Time: 5 ms (86.01%), Space: 59.8 MB (95.19%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.js
@@ -0,0 +1,24 @@
+var validPalindrome = function(s) {
+    let left = 0;
+    let right = s.length-1;
+
+    while(left<right){
+        if(s[left] !== s[right]){
+            return check(s, left+1, right)  ||  check(s, left, right-1)
+        }
+        left++
+        right--
+    }
+    return true
+};
+
+function check(s, left, right){
+    while(left<right){
+        if(s[left] !== s[right]){
+            return false
+        }
+        left++
+        right--
+    }
+    return true
+}
